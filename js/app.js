32b3271e484169bcf19132ed597bbb21a1fbe43d
gjohnn <juansgb316@gmail.com>
2022-11-21 17:56:41 -0300

    coso
    para la notebook

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//Variable, no puede comenzar con signos excepto _ ni números
+var nombre;
+var ape;
+var nombre_C;
+let apellido;
+nombre = "Juan";
+ape = "Guerrero";
+nombre_C = nombre + " " +ape;
+//Constante
+const IVA = 1.21;
+var preciofideo;
+preciofideo = 100;
+const total = preciofideo * IVA;
+const final = nombre_C +" debe pagar $"+ total;
+console.log(nombre)
+console.log(ape)
+console.log(nombre_C)
+console.log(final)
+
+//let ingnom = prompt("Ingresa tu nombre");
+//let ingape = prompt("Ingresa tu apeliido");
+//console.log(ingnom);
+//console.log(ingape);
+num1 = parseFloat(prompt("Ingresa el primer número"));
+num2 = parseFloat(prompt("Ingresa el segundo número"));
+resul= num1+num2
+console.log(resul)
+
+
